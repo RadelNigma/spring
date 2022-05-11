1725b7764981ad3eedb2b9147c3175e1c79dbe12
RadelNigma <radelnigma@yandex.ru>
2022-05-12 02:32:26 +0300

    commit2

--- src/main/java/com/boots/service/UserService.java
+++ src/main/java/com/boots/service/UserService.java
@@ -1,12 +1,75 @@
 package com.boots.service;
 
+import com.boots.entity.Role;
+import com.boots.entity.User;
+import com.boots.repository.RoleRepository;
+import com.boots.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Collections;
+import java.util.List;
+
+@Service
 public class UserService implements UserDetailsService {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository repository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user;
+    }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElse(new User());
+    }
+
+    public List<User> allUsers() {
+        return userRepository.findAll();
+    }
+
+    public Boolean saveUser(User user) {
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+        if (userFromDB != null) {
+            return false;
+        }
+
+        user.setRoles(Collections.singleton(new Role(1L,"ROLE_USER")));
+        user.setPassword(bCryptPasswordEncoder.encode((user.getPassword())));
+        userRepository.save(user);
+        return true;
+    }
+
+    public Boolean deleteUser (Long userId) {
+        if (userRepository.findById(userId).isPresent()) {
+            userRepository.deleteById(userId);
+            return true;
+        }
+        return false;
+    }
+
+    public List<User> userGetList(Long idMin){
+        return em.createQuery("SELECT u FROM User u WHERE  u.id > :paramId",User.class)
+                .setParameter("paramId",idMin).getResultList();
     }
 }
